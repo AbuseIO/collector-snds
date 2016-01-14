54b1ed6c2f2003af956db065e295ab409b93bf92
Bart Vrancken <bart@abuse.io>
2016-01-14 21:24:56 +0100

    refactor event to incident to prevent model confusion

--- src/Snds.php
+++ src/Snds.php
@@ -116,13 +116,13 @@ class Snds extends Collector
                                 "{$this->configBase}.feeds.{$this->feedName}.type"
                             );
                             /*
-                             * This prevents multiple events on the same day. So info
+                             * This prevents multiple incidents on the same day. So info
                              * blob has a scan time and this a report time
                              */
                             $incident->timestamp   = strtotime('0:00');
                             $incident->information = json_encode($report);
 
-                            $this->events[] = $incident;
+                            $this->incidents[] = $incident;
                         }
                     }
                 }
